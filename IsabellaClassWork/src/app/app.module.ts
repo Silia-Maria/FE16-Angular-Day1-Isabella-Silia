fb03e225d1d0c986e6bb911ea798d998a7720cfc
Isabella W <isabellawindhaber@gmail.com>
2022-10-10 11:03:24 +0200

    Home and About Page

--- IsabellaClassWork/src/app/app.module.ts
+++ IsabellaClassWork/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
