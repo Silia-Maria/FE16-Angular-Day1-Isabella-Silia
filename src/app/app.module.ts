34eced53fd4c9a2519320745e6fddbee6deb15e2
Silia Cronauer <silia.maria@web.de>
2022-10-10 14:01:50 +0200

    homepage almost done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HeroComponent } from './hero/hero.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HeroComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
